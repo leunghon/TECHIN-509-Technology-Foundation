5f615863f86245a8388979e18ed2262aceb3ff3b
leunghon <104844921+leunghon@users.noreply.github.com>
2022-11-05 21:33:55 -0700

    starter code

--- tictactoe_week5/cli.py
+++ tictactoe_week5/cli.py
@@ -5,5 +5,9 @@ if __name__ == '__main__':
     board = make_empty_board()
     winner = None
     while winner == None:
-        print('TODO: take a turn!')
-        
+        print("TODO: take a turn!")
+        # TODO: Show the board to the user.
+        # TODO: Input a move from the player.
+        # TODO: Update the board.
+        # TODO: Update who's turn it is.
+        winner = 'X'  # FIXME
