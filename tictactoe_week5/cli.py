68f80f8ffd4b83dfa60d6fe49a5f0afb12553f59
leunghon <104844921+leunghon@users.noreply.github.com>
2022-11-29 00:28:26 -0800

    added csv

--- tictactoe_week5/cli.py
+++ tictactoe_week5/cli.py
@@ -1,23 +1,44 @@
 #from logic import make_empty_board
 import logic
+import csv
+import pandas as pd
+import uuid
 
+
+def read_games():
+    try:
+        return pd.read_csv('games.csv')
+    except FileNotFoundError:
+        return pd.DataFrame(columns=[
+            'Game ID',
+            'Player A',
+            'Player B',
+            'Winner',
+            'Total Moves'
+        ])
+games = read_games()
+games.to_csv('/Users/leunghon/Documents/文件 - Hon的MacBook Pro - 1/Leung Hon/GIX/TECHIN 509 Technology Foundation/tictactoe_week5/games.csv')
 
 if __name__ == '__main__':
     board = logic.make_empty_board()
     winner = None
     player = None
+    moves = 0
     if player == None:
         player_with_bot = input('Are you playing alone? Please enter Y or N')
         if player_with_bot == 'N':
+            player_name_a = input('Please enter your name')
             player = input('Please pick either X or O to play the game')
-            print('Player 1, you are ', player)
+            print(player_name_a, ' you are ', player)
             player_b = logic.other_player(player)
-            print('Player 2, you are ', player_b)
+            player_name_b = input('Please enter your name')
+            print(player_name_b, ' you are ', player_b)
             turn = player
             while winner == None:
                 if logic.board_with_space(board) is False:
                     winner = 'Even'
                     print('This game ends in a draw')
+                    games.loc[len(games.index)] = ({'Game ID' : str(uuid.uuid4()), 'Player A' : player_name_a, 'Player B' : player_name_b, 'Winner' : winner, 'Total Moves' : moves})
                 else:
                     print(turn, "TODO: take a turn!")
                     print('This is the current board', board)
@@ -26,9 +47,11 @@ if __name__ == '__main__':
                     if logic.empty_spot(board, player_row_choice, player_col_choice) is True:
                         logic.make_move(board, turn, player_row_choice, player_col_choice)
                         print('Here is the updated board: ', board)
+                        moves += 1
                         if logic.get_winner(board) is not None:
                             winner = logic.get_winner(board)
                             print('Congratulations. ', winner, ' has won the game!')
+                            games.loc[len(games.index)] = ({'Game ID' : str(uuid.uuid4()), 'Player A' : player_name_a, 'Player B' : player_name_b, 'Winner' : winner, 'Total Moves' : moves})
                         else:
                             if turn == player:
                                 turn = player_b
@@ -37,15 +60,18 @@ if __name__ == '__main__':
                     else:
                         print('Please re-enter your turn.')
         elif player_with_bot == 'Y':
+            player_name_a = input('Please enter your name')
             player = input('Please pick either X or O to play the game')
-            print('Player 1, you are ', player)
+            print(player_name_a, ' you are ', player)
             Bot = logic.bot(logic.other_player(player))
+            player_name_b = "Bot"
             player_b = Bot.name()
             turn = player
             while winner == None:
                 if logic.board_with_space(board) is False:
                     winner = 'Even'
                     print('This game ends in a draw')
+                    games.loc[len(games.index)] = ({'Game ID' : str(uuid.uuid4()), 'Player A' : player_name_a, 'Player B' : player_name_b, 'Winner' : winner, 'Total Moves' : moves})
                 else:
                     if turn == player:
                         print(turn, "TODO: take a turn!")
@@ -55,9 +81,11 @@ if __name__ == '__main__':
                         if logic.empty_spot(board, player_row_choice, player_col_choice) is True:
                             logic.make_move(board, turn, player_row_choice, player_col_choice)
                             print('Here is the updated board: ', board)
+                            moves += 1
                             if logic.get_winner(board) is not None:
                                 winner = logic.get_winner(board)
                                 print('Congratulations. ', winner, ' has won the game!')
+                                games.loc[len(games.index)] = ({'Game ID' : str(uuid.uuid4()), 'Player A' : player_name_a, 'Player B' : player_name_b, 'Winner' : winner, 'Total Moves' : moves})
                             else:
                                 turn = player_b
                         else:
@@ -65,11 +93,18 @@ if __name__ == '__main__':
                     elif turn == player_b:
                         logic.bot.move(board, player_b)
                         print('Here is the updated board: ', board)
+                        moves += 1
                         if logic.get_winner(board) is not None:
                             winner = logic.get_winner(board)
                             print('Congratulations. ', winner, ' has won the game!')
+                            games.loc[len(games.index)] = ({'Game ID' : str(uuid.uuid4()), 'Player A' : player_name_a, 'Player B' : player_name_b, 'Winner' : winner, 'Total Moves' : moves})
                         else:
                             turn = player
+
+
+
+
+
 
         # TODO: Show the board to the user.
         # TODO: Input a move from the player.
